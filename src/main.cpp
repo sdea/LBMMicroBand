b4a4615fb1a174672343bc906a39f3533a7dc640
sdea <sdea@Salvatores-MacBook-Pro.local>
2018-01-25 13:37:19 +0100

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "palabos2D.h"
+#include "palabos2D.hh"
+
+using namespace plb;
+typedef double T;
+
+int main(int argc, char* argv []) {
+
+	// Init palabos simulation
+    	plbInit(&argc, &argv);
+	pcout << "Hello palabos" << std::endl;
+
+
+
+
+}
